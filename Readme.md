709cff6e4639bac311dcb103ec7f8c25665e7c36
bigyan <bigyanrai@gmail.com>
2012-09-06 11:44:25 -0400

    "Hello World"

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Java Hello World Application:
+__________________________
+
+What is it ?
+____________
+
+It is a simple java program which print "Hello World" on console.
---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+package helloworld;
+
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorld {
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
